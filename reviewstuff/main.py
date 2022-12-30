76d440b8c5728c9f87eaeb8add169fb2da04bc92
Nathan Pucheril <nathanpucheril@users.noreply.github.com>
2022-12-30 01:36:36 -0700

    Fix render gunicorn setup

--- reviewstuff/main.py
+++ reviewstuff/main.py
@@ -1,5 +1,32 @@
-import reviewstuff
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from reviewstuff import routes
+import os
+
+
+# create the extension
+db = SQLAlchemy()
+
+
+def create_app():
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    # create the app
+    app = Flask(__name__, instance_relative_config=False)
+
+    # configure the SQLite database, relative to the app instance folder
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, '../database/app.db')
+
+    db.init_app(app)
+
+    routes.routes(app, db)
+
+    app.db = db
+
+    return app
+
 
 if __name__ == '__main__':
-    app = reviewstuff.create_app()
+    app = create_app()
     app.run(debug=True)
---
+++ reviewstuff/wsgi.py
@@ -0,0 +1,10 @@
+import main
+
+
+def create_app():
+    return create_app()
+
+
+if __name__ == '__main__':
+    app = main.create_app()
+    app.run()
